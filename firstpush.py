15f284723fe65f2c448b0917752a89441a5b3dca
Loran Bogaart <loranbogaart@outlook.com>
2021-09-03 13:06:13 +0200

    Some sums

--- firstpush.py
+++ firstpush.py
@@ -1,1 +1,8 @@
-"# TIL6010-Assignments" 
+## "" is called a string
+## a value like 2021 is an integer. A string can be transformed with int("string")
+##float() is decimal number like 10.1, bool(), str() value --> string
+
+first = input("first: ")
+second = input("second: ")
+sum = int(first) + int(second)
+print(sum)
